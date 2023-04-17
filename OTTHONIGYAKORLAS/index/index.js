45ac7241f367848aaa0ffe71341b8646a84bf673
szabomarcel <szabomarcell34@gmail.com>
2023-04-17 10:47:15 +0200

    main

---
+++ OTTHONIGYAKORLAS/index/index.js
@@ -0,0 +1,44 @@
+// Komment(line comment)
+
+/* 
+Block komment
+fetch("https://jsonplaceholfrt.typicode.com/posts")
+.then(res => res.json)
+.then(res => console.log(res))
+*/
+
+/*
+* Adattípusok
+*/
+
+// string(karakterfűzér)
+console.log("Szia"); // Szint taktika
+console.log("Szia");
+
+// együk nagy betűssé
+// Nézzük meg hány larakterből áll
+// fordítsunk (reverse)
+// fűzzünk hozzá egy másik stirnget
+
+// Number (szám)
+console.log(500);
+
+// Adjun hozzá egy másik számot
+// Vonjunk ki belőle
+//osztás utáni maradék
+
+// boolean(igaz hamos értékekreprezentáló típus)
+console.log(true);
+console.log(false);
+
+// Logikai és
+// (boolean, boolean) -> boolean
+
+// negálás 
+// ...
+
+// undefind (egy értéké nincs a rendszerünkbe definiálva)
+console.log(undefined)
+
+// null
+console.log(null)
