5f40c0e30a1bc944490f0aece1106ec5453a6592
szabomarcel <szabomarcell34@gmail.com>
2023-04-18 10:16:27 +0200

    main

--- OTTHONIGYAKORLAS/index/index.js
+++ OTTHONIGYAKORLAS/index/index.js
@@ -38,7 +38,175 @@ console.log(false);
 // ...
 
 // undefind (egy értéké nincs a rendszerünkbe definiálva)
-console.log(undefined)
+console.log(undefined);
 
 // null
-console.log(null)
+console.log(null);
+
+/*
+*Array(tömb)
+*/
+
+// Array<_> 
+// Array of ?
+// Array<string>
+
+console.log("elso","második", "harmadik");
+
+// Array<number>
+console.log([2, 12, 3]);
+
+// szűrés
+// sorba rendezés
+// új elem hozzáadás
+// aggregálás
+
+console.log([2, 12, 3][1]);
+
+// nem javasolt tömb
+console.log([true, 2, "string..."])
+
+// több dimenziós tömb
+// Array<Array<number>>
+console.log([[2,3], [32, 10], [6, 4]][1][1])
+
+/*
+*Object(objektum)
+*/
+
+// kulcs érték párok sorozata
+console.log(
+    {
+        firstName: 'József',
+        lastName: 'Kovács',
+        age: 32
+    }.lastName
+);
+
+console.log(
+    {
+        firstName: 'József',
+        lastName: 'Kovács',
+        age: 32
+    }['lastName']
+);
+
+// lookup, kulcs alapú kikérés
+console.log(
+    {
+        firstName: 'József',
+        lastName: 'Kovács',
+        age: 32
+    }.lastName
+);
+
+console.log(
+    {
+        firstName: 'József',
+        lastName: 'Kovács',
+        phoneNumbers: ['01023143', '1231132'],
+        age: 32,address: {
+            street: "Király u",
+            city: "Debrecen"
+        }
+    }.phoneNumbers[0]
+);
+
+// 1. Az első telefon neve
+console.log(
+    [
+        {
+            brand: 'Apple',
+            name: 'iPhone X',
+            price: 250000,
+            provider:{
+                country: 'HU',
+                name:"Vodafone"
+            }
+        },
+        {
+            brand: 'Huawei',
+            name: '10',
+            price: 180000,
+            provider:{
+                country: 'HU',
+                name:"T-Mobile"
+            }
+        },
+        {
+            brand: 'Samsung',
+            name: 'A53',
+            price: 300000,
+            provider:{
+                country: 'HU',
+                name:"Yettel"
+            }
+        }
+    ][0].name
+)
+
+// 2. A második telefon ára
+console.log(
+    [
+        {
+            brand: 'Apple',
+            name: 'iPhone X',
+            price: 250000,
+            provider:{
+                country: 'HU',
+                name:"Vodafone"
+            }
+        },
+        {
+            brand: 'Huawei',
+            name: '10',
+            price: 180000,
+            provider:{
+                country: 'HU',
+                name:"T-Mobile"
+            }
+        },
+        {
+            brand: 'Samsung',
+            name: 'A53',
+            price: 300000,
+            provider:{
+                country: 'HU',
+                name:"Yettel"
+            }
+        }
+    ][1].price
+)
+
+// 3. A harmadik telefon szolgáltatójának országa
+console.log(
+    [
+        {
+            brand: 'Apple',
+            name: 'iPhone X',
+            price: 250000,
+            provider:{
+                country: 'HU',
+                name:"Vodafone"
+            }
+        },
+        {
+            brand: 'Huawei',
+            name: '10',
+            price: 180000,
+            provider:{
+                country: 'HU',
+                name:"T-Mobile"
+            }
+        },
+        {
+            brand: 'Samsung',
+            name: 'A53',
+            price: 300000,
+            provider:{
+                country: 'HU',
+                name:"Yettel"
+            }
+        }
+    ][2].provider.country
+)
