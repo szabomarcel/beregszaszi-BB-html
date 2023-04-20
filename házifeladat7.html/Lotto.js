0e91a3718a80d7a52cabf875b771791b4e553ae4
szabomarcel <szabomarcell34@gmail.com>
2023-04-20 10:43:22 +0200

    main

--- házifeladat7.html/Lotto.js
+++ házifeladat7.html/Lotto.js
@@ -76,5 +76,16 @@ for (var i = 0; i < cellak.length; i++) {
     
 // táblázat generalasa
 
-
+function* generator(i) {
+    yield i;
+    yield i + 10;
+  }
+  
+  const gen = generator(10);
+  
+  console.log(gen.next().value);
+  // Expected output: 10
+  
+  console.log(gen.next().value);
+  // Expected output: 20
 
