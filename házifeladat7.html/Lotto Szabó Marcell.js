20c5c2c5bda3059cf92dc540e057f86950eff81e
szabomarcel <szabomarcell34@gmail.com>
2023-04-25 20:57:26 +0200

    main

--- házifeladat7.html/Lotto Szabó Marcell.js
+++ házifeladat7.html/Lotto Szabó Marcell.js
@@ -136,17 +136,6 @@ document.write("</table>");
 */
 
 /*
-let x, y, z;
-document.getElementById("rollButton").onclick = function(){
-    x = Math.floor(Math.random() * 6) + 1;
-    y = Math.floor(Math.random() * 6) + 1;
-    z = Math.floor(Math.random() * 6) + 1;
-
-    document.getElementById("xlabel").innerHTML = x;
-    document.getElementById("ylabel").innerHTML = y;
-    document.getElementById("zlabel").innerHTML = z;
-}
-
 function* generatorFunc() {
     yield 100;
 }
