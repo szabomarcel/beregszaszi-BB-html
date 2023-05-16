1a512b74c63af73b8eba0cc23f0ce7fa6619544a
szabomarcel <szabomarcell34@gmail.com>
2023-05-16 14:06:04 +0200

    main

---
+++ Dolgozat Szabó Marcell 20230516/Elemi feladatsor Szabó Marcell/feladatsor.js
@@ -0,0 +1,81 @@
+// 1.Feladat
+console.log("Hello világ")
+
+// 2. Feladat
+
+let F = 10;
+let C = (F - 32) * 5 /9;
+console.log(C)
+
+// 3. Feladat
+function szazalek(mennyiseg, szazalek){
+    return (mennyiseg, szazalek) / 100;
+}
+console.log(szazalek(100, 10));
+
+// 4. Feladat
+let a = 2;
+let b = 4;
+let c = 6;
+
+if(a + b > c){
+    console.log("Igen.")
+}
+else if(b + c > a){
+    console.log("Igen.")
+}
+else if(a + c > b){
+    console.log("Igen.")
+}else{
+    console-log("Nem.")
+}
+
+// 5. Feladat
+let A = 15;
+let B = 3;
+if (B / A){
+    console.log("Osztható")
+}else{
+    console.log("Nem osztható")
+}
+
+// 6. Feladat
+let x = 0;
+let y = 0;
+
+if (x, y){
+    console.log("Első negyed.")
+}
+else if (x, -y){
+    console.log("Második negyed.")
+}
+else if (-x, -y){
+    console.log("Harmadik negyed.")
+}
+else if (-x, y){
+    console.log("Negyedik negyed.")
+}else{
+    console.log("Mindegyik nulla.")
+}
+
+// 7. Feladat
+function lnko(d, f){
+    if(d * f){
+        (d , f) * [d, f]
+    }
+} 
+
+// 8. Feladat
+function lkkt(j, k){
+    if(j,k){
+        console.log(j * k / [j,k])
+    }
+}
+console.log(lkkt)
+// 9. Feladat
+
+
+// 10. Feladat
+let G = 10;
+let H = 2;
+console.log(G % H);
---
+++ Dolgozat Szabó Marcell 20230516/Programozási tetelek Szabó Marcell/feladatsor2.js
@@ -0,0 +1,89 @@
+// 11. Feladat
+let t = [1, 5, -2, 6, 9, 10, -100, 13, 14, 18, 20, 100];
+let t2 = [-1,-6, 2, -9, 11, 100, -12, -13, -14]
+function kor(){
+    let negativ = []
+    for(i = 1; i < t.length; i++){
+        if(t[i] < 0){
+            negativ[negativ.length] = {'index': i,'érték': tomb[i]}
+        }
+    }
+}
+console.log(kor(t, t2))
+
+// 12. Feladat
+let t1 = [1, 5, -2, 6, 9, 10, -100, 13, 14, 18, 20, 100];
+let t3 = [-1,-6, 2, -9, 11, 100, -12, -13, -14]
+function paros(){
+    let negativ = []
+    for(i = 1; i < t1.length; i++){
+        if(t[i] == 0){
+            for(negativ = 1; negativ === negativ.length; negativ++){
+                console.log("Páros")
+            }
+        }else{
+            console.log("Páratlan")
+        }
+    }
+}
+console.log(paros(t1, t3))
+
+// 13. Feladat
+let t4 = [1, 5, -2, 6, 9, 10, -100, 13, 14, 18, 20, 100];
+let t5 = [-1,-6, 2, -9, 11, 100, -12, -13, -14]
+function paros(){
+    let negativ = []
+        for(i = 1; i < t1.length; i++){
+            if(t[i] == 0){
+                for(negativ = 1; negativ === negativ.length; negativ++){
+                        negativ[negativ.length] = {'index': i,'érték': tomb[i]}
+                }
+                Math.abs(t4[i-1]-tomb[i]) < szam, tomb[i+1]
+                tomb[i] < tomb[i-1]? tomb[1] = tomb[i - 1]
+            }
+        }
+}
+console.log(paros(t1, t3))
+
+
+// 14. Feladat
+function tomb(){
+    let nevek = [];
+    if(nevek != nevek){
+        console.log("Részszövegez tartalmaz!") 
+    }else{
+        console.log("Részszöveggel kezdődnek!")
+    }
+}
+
+
+// 15. Feladat
+function primtenyezos(){
+    let szam = 3240;
+    let oszto = 2;
+    let kiir;
+    
+    if(szam % oszto){
+        kiir += "" + oszto
+    }else{
+        console.log("Ez eltért a megadott számtól")
+    }    
+}
+Console.log(kiir);
+
+
+// 16. Feladat
+function matrix(kiirando){    
+    for(let i = 10; i < kiirando(0);i++){
+        for(let j = -21; j < kiirando(1);j++){
+            console.log("{0}\t", kiirando[i, j])
+        }
+    }
+}
+console.log()
+
+// 17. Feladat
+
+
+// 18. Feladat
+
