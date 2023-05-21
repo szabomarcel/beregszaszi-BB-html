6e02d0eb301c99d43440a44a85b5e69e16469622
szabomarcel <szabomarcell34@gmail.com>
2023-05-21 20:15:02 +0200

    main

---
+++ OTTHONIGYAKORLAS/Gyakorlas/dolgozatosgyak.js
@@ -0,0 +1,76 @@
+// 1. Feladat
+console.log("Helló Világ");
+
+// 2. Feladat
+let F;
+let C = (F - 32) * 5 / 9;
+console.log(C);
+
+// 3. Feladat
+function szazalek(mennyiseg, szazalek){
+    return(mennyiseg, szazalek) / 100;
+}
+
+// 4. Feladat
+let c = 2;
+let h = 6;
+let g = 8;
+
+if(c + h > g){
+    console.log("Igen")
+}else if(c + g > h){
+    console.log("Igen")
+}else if(h + g > c){
+    console.log("Igen")
+}else{
+    console.log("Nem")
+}
+
+// 5. Feladat
+let A = 12;
+let B = 4;
+
+if(A / B){
+    console.log("Osztható")
+}else{
+    console.log("Nem osztható")
+}
+
+// 6. Feladat
+function sikpont(){
+    let x = 10;
+    let y = 9;
+
+    if(y, x){
+        console.log("Első negyedben van.")
+    }else if(-y, x){
+        console.log("Második negyedben van.")
+    }else if(-y, -x){
+        console.log("Harmadik negyedben van.")
+    }
+    else if(y, -x){
+        console.log("Negyedik negyedben van.")
+    }
+}
+
+// 7. Feladat
+function lnko(d, f){
+    if(d * f){
+        (d, f) * [d, f]
+    }
+}
+
+// 8. Feladat
+function lkkt(j, k){
+    if(j, k){
+        console.log(j * k / [j, k])
+    }
+}
+
+// 9. Feladat
+
+// 10. Feladat
+let a = 20;
+let b = 10;
+
+console.log(a % b);
