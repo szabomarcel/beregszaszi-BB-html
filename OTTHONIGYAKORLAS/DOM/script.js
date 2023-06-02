5b260529db4f1dc457e276e21c3b3f8d609bb081
szabomarcel <szabomarcell34@gmail.com>
2023-06-02 10:51:59 +0200

    main

---
+++ OTTHONIGYAKORLAS/DOM/script.js
@@ -0,0 +1,37 @@
+
+const body = document.body;
+const div = document.body;
+const spanHi = document.querySelector('#hi');
+const spanBye = document.querySelector('#bye');
+
+
+spanHi.classList.toogle("h2", false)
+spanHi.classList.remove("hi")
+//console.log(spanHi.setAttribute("title","asdasasdr")); 
+
+//spanHi.remove();
+
+//div.remove(spanHi);
+
+/*
+spanBye.remove();
+div.append(spanBye);
+*/
+
+// body.append("Hello Word","bye")
+
+/*
+const div = document.createElement("div");
+const strong = document.createElement("strong");
+strong.innerText = "Hello World 2";
+// div.innerText = "Hello World";
+// div.innerHTML = "<strong>Hello World 2</spang>";
+div.append(strong);
+body.append(div);
+*/
+
+/*
+document.querySelector("div");
+console.log(div.textContent);
+console.log(div.innerTexts);
+*/
