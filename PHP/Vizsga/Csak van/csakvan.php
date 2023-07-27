d3b5daa1c71cb45a8e5458b79e6d1474a99882b8
szabomarcel <szabomarcell34@gmail.com>
2023-07-27 09:49:43 +0200

    main

---
+++ PHP/Vizsga/Csak van/csakvan.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="hu">
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="icon" href="/favicon.ico"/>
+        <meta name="theme-color" content="#FFFFFF"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <meta name="description" content="MENSTAGE"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <link rel="apple-touch-icon" href="/logo192.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <title>A legjobb focizásra való jelentkezés</title>
+        <style>
+            body,html{background-color:#fff;min-height:100%}
+            #root{display:flex;justify-content:center;align-items:center;min-height:100%}
+            .logo{margin-top:200px;display:inline-block;animation:animateHeart .6s infinite alternate}
+            @keyframes animateHeart{to{transform:scale(1.4)}}
+        </style>
+        <script defer="defer" src="/static/js/main.d27e0f43.js"></script>
+        <link href="/static/css/main.712b5ce8.css" rel="stylesheet">
+    </head>
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root"><img class="logo" width="200" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeBm26yOtBiCpUnhJxa8K8gR9My5OzNPG5tg"/></div>
+    </body>
+</html>
